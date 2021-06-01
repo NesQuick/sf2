1654ae343bdb7c60134f80c4e3a37a2069946b7e
NesQuick <kiyatkin.misha@gmail.com>
2021-06-02 00:36:02 +0300

    added wind speed

--- wind_detection.py
+++ wind_detection.py
@@ -1,10 +1,36 @@
-import smbus2 as smbus
+import smbus
 import time
 
-DEVICE_BUS = 1
-DEVICE_ADDR = 0x11
+i2cbus = smbus.SMBus(1)
+i2caddress = 0x11
+	
+PROSHIVKA = 0x04 #version
+IDENT = 0x05 #identifier
+STATUS = 0x06 #status
+ATEMPCOLD = 0x09 #temps of cold ACP
+ATEMPHOT = 0x0B #temps of hot ACP
+SPEED = 0x07 #wind speed
+VOLTIN = 0x0D #input voltage
+PHEAT = 0x0E #heating power
+TEMPCOLD = 0x10 #temps of cold
+TEMPHOT = 0x12 #temps of hot
+TEMPDIFDEV = 0x14 #difference of temps on device
+TEMPDIF = 0x10 #out temps, seems like the same as TEMPCOLD
+
+TEMP_COLD_H = 0x10
+TEMP_COLD_L = 0x11
+WIND_H = 0x07
+WIND_L = 0x08
+	
 while True:
-	bus = smbus.SMBus(DEVICE_BUS)
-	print(bus.write_byte_data(DEVICE_ADDR, 0x00, 0x01))
-	time.sleep(0.5)
+	#print(i2cbus.read_byte_data(i2caddress, VOLTIN))
+	#h = i2cbus.read_byte_data(i2caddress, TEMP_COLD_H)
+	#l = i2cbus.read_byte_data(i2caddress, TEMP_COLD_L)
+	h = i2cbus.read_byte_data(i2caddress, WIND_H)
+	l = i2cbus.read_byte_data(i2caddress, WIND_L)
+	tmp = (h << 8) | l
+	tmp_out = tmp/10
+	print(tmp_out)
+	time.sleep(1)
+
 
